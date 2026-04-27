e9e9e79fec1be445b1c06a4c0cef8060e584816f
dashvorobyshek <v0rdarja@yandex.ru>
2026-04-28 01:09:58 +0300

    fix

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,6 +1,5 @@
 from rest_framework import viewsets, filters
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
 from posts.models import Post, Group, Comment, Follow
 from .serializers import (
